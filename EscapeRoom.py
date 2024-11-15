f6c98a007bffc2c49bc8c2a8748add563103d111
jillianw14 <jillianw14@nycstudents.net>
2024-11-15 10:06:45 -0500

    d

--- EscapeRoom.py
+++ EscapeRoom.py
@@ -1,6 +1,7 @@
-from PIL import Image 
- 
-# creating a object 
-im = Image.open(r"c:\Users\jillian.weingarten24\Desktop\escape-room.jpg") 
- 
-im.show()
+if player == "__main__":
+    while True:
+        print("Welcome to the Neon Nexus, a sprawling megacity where neon lights flicker against the backdrop of rain-soaked streets.")
+        print("You and your team are a group of underground hackers who have stumbled upon a corporation's dark secrets.")
+        print("The megacorp has caught wind of your investigation and has locked you inside their secure facility.")
+        print("Your mission is to escape from three high-security rooms before the corporate enforcers arrive to silence you permanently.")
+        print("Time to get started!")
--- openingimage.py
+++
@@ -1,1 +0,0 @@
-c:\Users\jillian.weingarten24\Desktop\escape-room.jpg
