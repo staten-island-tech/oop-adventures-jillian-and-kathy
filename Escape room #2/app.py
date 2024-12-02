ab81d201a01693cdbd662b5ad237bc328d8b9fb2
Cathyyu10 <cathyy2@nycstudents.net>
2024-12-02 10:15:28 -0500

    t.done

--- Escape room #2/app.py
+++ Escape room #2/app.py
@@ -1,5 +1,8 @@
 import pygame
 import sys
 
+screen_width = 1000
+screen_length = 600
+
 pygame.init()
-background = pygame.image.load('escape room 2.jpg')
+background = pygame.image.load('escape room 2.jpg'), (screen_width, screen_length)
