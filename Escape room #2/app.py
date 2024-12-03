9a300217d7c61b3751e221a485240566fcc5a0b4
Cathyyu10 <cathyy2@nycstudents.net>
2024-12-03 10:14:19 -0500

    d

--- Escape room #2/app.py
+++ Escape room #2/app.py
@@ -6,6 +6,7 @@ pygame.init()
 
 WIDTH = 1000
 HEIGHT = 600
+
 background = pygame.image.load('image.jpg')
 background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -13,42 +14,3 @@ pygame.display.set_caption("Escape room #2")
 
 font = pygame.font.Font(None, 36)
 
-def slow_print(text, delay=0.05):
-    """Prints text to the screen slowly, character by character."""
-    current_text = ""
-    for char in text:
-        current_text += char
-        screen.blit(background_image, (0, 0))
-        rendered_text = font.render(current_text, True, (255, 255, 255))
-        screen.blit(rendered_text, (50, 50))
-        pygame.display.flip()
-        time.sleep(delay)
-
-def main():
-<<<<<<< Updated upstream
-    game = game()
-=======
-    game = Game()
->>>>>>> Stashed changes
-    text_display = TextDisplay(game)
-    quotes = [
-        "Congrats! You've completed the first escape room"
-    ]
-
-    for quotes in quots:
-        slow_print(quote, 0.05)
-        time.sleep(1)
-        screen.blit(background_image, (0, 0))
-        pygame.display.flip()
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type ==pygame.QUIT:
-                running = False
-    
-    pygame.quit()
-    sys.exit()
-
-if __name__ == "__main__":
-    main()
