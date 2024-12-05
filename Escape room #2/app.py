4fa70b6f2b3466add590f3c0a661255806b3ecb4
Cathyyu10 <cathyy2@nycstudents.net>
2024-12-05 10:15:11 -0500

    asdasdasd

--- Escape room #2/app.py
+++ Escape room #2/app.py
@@ -28,7 +28,8 @@ def slow_print(text, delay=0.01):
 
 def main():
     quotes = [
-        "Congrats! You've completed the first room, ",
+        "Welcome to the second room,"
+        "you only have x minutes to find all of the keys and escape"
         
     ]
 
@@ -43,12 +44,74 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                pygame.quit()
+                sys.exit()
+if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    mouse_x, mouse_y = pygame.mouse.get_pos() 
+                    if clickable_area1.collidepoint(mouse_x, mouse_y):
+                        slow_print("You clicked on the TV monitor!", 0.03)
+                        time.sleep(0.5)
+                        screen.blit(background_image, (0, 0))  
+                        pygame.draw.rect(screen, (255, 0, 0), clickable_area1, 3) 
+                        pygame.display.flip()
 
 
-    pygame.quit()
-    sys.exit()
+                    if clickable_area2.collidepoint(mouse_x, mouse_y):
+                        slow_print("You clicked on the food plate!", 0.03)
+                        time.sleep(0.5)
+                        screen.blit(background_image, (0, 0))  
+                        pygame.draw.rect(screen, (255, 0, 0), clickable_area2, 3)  
+                        pygame.display.flip()
+pygame.draw.rect(screen, (255, 0, 0), clickable_area1, 3) 
+pygame.draw.rect(screen, (255, 0, 0), clickable_area2, 3) 
+pygame.display.flip()
+
+
+
 
 if __name__ == "__main__":
     main()
 
+ 
 
+#first puzzle
+""" class safe_puzzle():
+    def guess_code():
+        first_digit = 1
+        second_digit = 2
+        third_digit = 2
+        fourth_digit = 9
+        first = ("Guess the first digit: ")
+        if 'first' == first_digit:
+            print('Correct!')
+            if 'first' < first_digit:
+                print('Incorrect! Guess higher.')
+                if 'first' > first_digit:
+                    print('Incorrect! Guess lower.')
+                    second = ("Guess the second digit: ")
+                    if 'second' == second_digit:
+                        print('Correct')
+                        if 'second' < second_digit:
+                            print('Incorrect! Guess higher.')
+                            if 'second' > sceond_digit:
+                                print('Incorrect! Guess lower.')
+                                if 'third' == third_digit:
+                                    print('Correct')
+                                    if 'third' < third_digit:
+                                        print('Incorrect! Guess higher.')
+                                        if 'third' > third_digit:
+                                            print('Incorrect! Guess lower.')
+                                            if 'fourth' == fourth_digit:
+                                                print('Correct')
+                                                if 'fourth' < fourth_digit:
+                                                    print('Incorrect! Guess higher.')
+                                                    if 'fourth' > fourth_digit:
+                                                        print('Incorrect! Guess lower.')
+    guess_code() """
+
+
+    
+
+
+    
