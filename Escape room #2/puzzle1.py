2f3338a86a161f42ce82b270719a97d752d72c3c
Cathyyu10 <cathyy2@nycstudents.net>
2024-12-10 10:15:15 -0500

    sadas

--- Escape room #2/puzzle1.py
+++ Escape room #2/puzzle1.py
@@ -1,35 +1,75 @@
-first_digit = 1
-second_digit = 2
-third_digit = 2
-fourth_digit = 9
-first = input("Guess the first digit: ")
-if first == first_digit:
-    print('Correct!')
-    if first < first_digit:
-        print('Incorrect! Guess higher.')
-    if first > first_digit:
-        print('Incorrect! Guess lower.')
+digit1 = 1
+digit2 = 2
+digit3 = 2
+digit4 = 9
 
-second = input("Guess the second digit: ")
-if second == second_digit:
-    print('Correct')
-    if second < second_digit:
-        print('Incorrect! Guess higher.')
-        if second > second_digit:
-            print('Incorrect! Guess lower.')
 
-third = input('Guess the third digit: ')
-if third == third_digit:
-    print('Correct')
-    if third < third_digit:
-        print('Incorrect! Guess higher.')
-        if third > third_digit:
-            print('Incorrect! Guess lower.')
 
-fourth = input('Guess the fourth digit: ')
-if fourth == fourth_digit:
-    print('Correct')
-    if fourth < fourth_digit:
-        print('Incorrect! Guess higher.')
-        if fourth > fourth_digit:
-            print('Incorrect! Guess lower')
+guess1 = int(input("Guess a number from 1-10: "))
+history1 = []
+if guess1 != digit1:       
+    history1.append(guess1)
+    print (f'Guess history: {history1}')
+while digit1!= guess1:
+    if guess1 == digit1:
+        print('Your guess is correct!')
+        break
+    elif guess1 < digit1:
+        print('Your guess is too low! Try again!')
+        guess1 = int((input('Guess again: ')))
+        history1.append(guess1)
+        print(f'Guess history: {history1}')
+    elif guess1 > digit1:
+        print('Your guess is too high! Try again!')
+        guess1 = int((input('Guess again: ')))
+        history1.append(guess1)
+        print(f'Guess history: {history1}')
+    else:
+        print('Error. Try again!')
+if guess1 == digit1:
+    print('Your guess is correct!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" class firstpuzzle():
+    def guess_code1():
+        first = int(input("Guess the first digit: "))
+        first_history = []
+        if first != digit1:
+            first_history.append(first)
+            print(f'Guess history: {first_history}')
+        while digit1 != first:
+            if first < digit1:
+                print('Your guess is too low, guess again.')
+                first_again = int((input('Guess again: ')))
+                first_history.append(first_again)
+                print(f'Guess history: {first_history}')
+            elif first > digit1:
+                print('Your guess is too high, guess again.')
+                first_again = int((input('Guess again: ')))
+                first_history.append(first_again)
+                print(f'Guess history: {first_history}')
+            else:
+                print('Error. Try Again!')
+        if first_again == digit1:
+            print('Your guess is correct!')
+    guess_code1() """
