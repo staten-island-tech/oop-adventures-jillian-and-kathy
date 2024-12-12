9482960bdba93056740fb6dc1c2beb72ee3b157a
Victor-K-19 <victork33@nycstudents.net>
2024-12-12 10:18:33 -0500

    eth456

--- bookpuzzle.py
+++ bookpuzzle.py
@@ -2,6 +2,8 @@ import time
 """ import pygame """
 import sys
 
+lettercorrect = []
+lettertry = []
 word = "debug"
 infinite = True
 print("Your final puzzle is a Wordle")
@@ -10,6 +12,22 @@ while infinite == True:
     if enter == word:
         infinite = False
         print("The word is DEBUG! Congrats!!!")
+    elif len(enter) != len(word):
+        print("Must be 5 letter word")
+    elif len(enter) == len(word):
+        lettertry = []
+        for letter in range(len(enter)):
+            if enter[letter] == word[letter]:
+                print(enter[letter], "is correct and in the right spot")
+                lettercorrect.append(enter[letter])
+            elif enter[letter] in word and enter[letter] not in lettercorrect and enter[letter] not in lettertry:
+                lettertry.append(enter[letter])
+        for wrong_letter in lettertry:
+            print(wrong_letter, "is correct but in the wrong spot")
+        for letter in range(len(enter)):
+            if enter[letter] is not in word:
+                print(enter[letter], "is not in the word")
+
 
 
 """ class book():
