1a0ea33c1486b7342ce077da4e7dd678e85f6819
Cathyyu10 <cathyy2@nycstudents.net>
2024-12-16 10:16:01 -0500

    asdasd

--- Escape room #2/puzzle1.py
+++ Escape room #2/puzzle1.py
@@ -1,64 +1,80 @@
 import pygame
 import sys
 
-pygame.init()
-screen_width = 1920
-screen_height = 1017
-background_image = pygame.image.load('output.jpg')
-background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("safe")
-font = pygame.font.SysFont("Courier New", 50, bold = True)
 
-def slow_print(text, delay=0.01):
-    """Prints text to the screen slowly, character by character."""
-    current_text = ""
-    for char in text:
-        current_text += char
-        screen.blit(background_image, (0, 0))
-        rendered_text = font.render(current_text, True, (255, 255, 255))
-        screen.blit(rendered_text, (50, 50))
-        pygame.display.flip()
-        time.sleep(delay)
 
-def main():
-    quotes = [
-        "This is a safe, to open it you have to get the four-digit code right. I wonder whats inside?"
-        
-        
-    ]
 
-    for quote in quotes:
-        slow_print(quote, 0.01)  
-        time.sleep(1)  
-        screen.blit(background_image, (0, 0)) 
-        pygame.display.flip()
-    running = True
-    while running:
+
+
+
+class puzzle1():
+    def __init__(self):
+        pygame.init()
+        self.screen_width = 1920
+        self.screen_height = 1017
+        self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
+        self.messages =  ('This is a safe, to open it you have to get the four-digit code right. I wonder whats inside?')
+        pygame.display.set_caption("safe")
+        self.background_image = pygame.image.load('output.jpg')
+        self.background_image = pygame.transform.scale(self.background_image, (self.screen_width, self.screen_height))
+        self.font = pygame.font.SysFont("Courier New", 50, bold = True)
+        self.correct_digits = [1, 2, 2, 9]
+        self.current_digit = 0
+        self.guess_hisory = []
+        self.user_input = ''
+    def draw_screen(self):
+        self.screen.blit(self.background_image,(0, 0))
+        for i, message in enumerate(self.messages):
+            text = self.font.render(message, True, (255, 255, 255))
+            self.screen.blit(text, (50, 50 + i * 60))
+        digit_message = f'Guess the digit {self.current_digit + 1}: (Guess a number from 1-9)'
+        digit_text = self.font.render(digit_message, True, (255, 255, 255))
+        self.screen.blit(digit_text, (50, 400))
+        input_text = self.font.render(self.user_input, True, (255, 255, 0))
+        self.screen.blit(input_text, (50, 450))
+        history_text = f'Guess history: {self.guess_history}'
+        history_rendered = self.font.render(history_text, True, (255, 255, 255))
+        self.screen.blit(history_rendered, (50, 500))
+        pygame.display.update()
+    def handle_input(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    if self.user_input.isdigit() and self.user_input != '':
+                        guess = int(Self.user_input)
+                        self.guess_history.append(guess)
+                        self.check_guess(guess)
+                        self.user_input = ''
+                elif event.key == pygame.K_BACKSPACE:
+                    self.user_input = self.user_input[:-1]
+                else:
+                    self.user_input += event.unicode
+    def check_guess(self, guess):
+        if guess == self.correct_digits[self.current_digits]:
+            self.messages.append('Your guess is correct!')
+            self.current_digit += 1
+            if self.current_digit >= len(self.correct_digits):
+                self.messages.append("You've opened the safe!")
+        else:
+            if guess < self.correct_digits[self.current_digit]:
+                self.messages.append('Your guess is too low! Try again!')
+            else:
+                self.messages.append('Your guess is too high! Try again!')
+    def run(self):
+        while True:
+            self.handle_input()
+            self.draw_screen()
+if __name__ == '__main__':
+    game = puzzle1()
+    game.run()
 
-
-
-
-
-
-
-
-
-""" digit1 = 1
-digit2 = 2
-digit3 = 2
-digit4 = 9 
-class puzzle1():
-    print('This is a safe, to open it you have to get the four-digit code right. I wonder whats inside?')
-    def get_all_digits():
-        correct_digits = [digit1, digit2, digit3, digit4]
+    """ def get_all_digits():
         for i in range(4):
             print(f'Guess the digit {i + 1}:  (hint: use your resources)')
             guess = int(input("Guess a number from 1-10: "))
-            digit_history = []
             while guess != correct_digits[i]:       
                 digit_history.append(guess)
                 print (f'Guess history for digit {i + 1}: {digit_history}')
@@ -76,8 +92,9 @@ class puzzle1():
                     print('Error. Try again!')
                 if guess ==  correct_digits[i]:
                     print('Your guess is correct!')
-    get_all_digits()
+    get_all_digits()  
  """
+
 
 
 
