f3c83b75e17e21a3e458241c975afaf52041a289
Victor-K-19 <victork33@nycstudents.net>
2025-01-13 10:10:09 -0500

    wzrhxdfmg,

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
 import os
 
+class User:
+    def __init__(self, keys):
+        self.keys = keys
+        self.storage = []
+
+    def add_storage(self, key):
+        self.storage.append(key)
+        print(f"You have gained 1 {key}")
+    
+    def clear_storage(self):
+        self.storage = []
+        print(f"Your {self.keys} storage was cleared")
+
+    def show_storage(self):
+        print(f"This is your {self.keys} inventory: {self.storage}")
+
+inventory = User("Escape Keys")
+
+inventory.add_storage("Room 1 Key")
+
+inventory.add_storage("Room 2 Key")
+
+inventory.add_storage("Room 3 Key")
+
+inventory.show_storage()
+
+inventory.clear_storage()
+
+inventory.show_storage()
+
 # Target directory
 target_dir = 'C:\Users\jillian.weingarten24\Documents\GitHub\oop-adventures-jillian-and-kathy\Room_one\EscapeRoom.py'
 
