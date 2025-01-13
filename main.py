225dc010e64dc7e1a2098fdb8816610d9c13771c
Victor-K-19 <victork33@nycstudents.net>
2025-01-13 10:02:35 -0500

    rezhdt

--- main.py
+++ main.py
@@ -1,43 +1,3 @@
-<<<<<<< HEAD
-import time
-import pygame
-import sys
-from Escape_room3 import EscapeRoom3
-from Escape_room3 import bookpuzzle
-
-
-
-
-class User:
-    def __init__(self, keys):
-        self.keys = keys
-        self.storage = []
-
-    def add_storage(self, key):
-        self.storage.append(key)
-        print(f"You have gained 1 {key}")
-    
-    def clear_storage(self):
-        self.storage = []
-        print(f"Your {self.keys} storage was cleared")
-
-    def show_storage(self):
-        print(f"This is your {self.keys} inventory: {self.storage}")
-
-inventory = User("Escape Keys")
-
-inventory.add_storage("Room 1 Key")
-
-inventory.add_storage("Room 2 Key")
-
-inventory.add_storage("Room 3 Key")
-
-inventory.show_storage()
-
-inventory.clear_storage()
-
-inventory.show_storage()
-=======
 import os
 
 # Target directory
@@ -45,9 +5,9 @@ target_dir = 'C:\Users\jillian.weingarten24\Documents\GitHub\oop-adventures-jill
 
 # Dictionary of directories to iterate over
 directories = {
-    'Room_one': 'EscapeRoom.py',
-    'Room_two': 'app.py',
-    'Escape Room #3': 'EscapeRoom3.py'
+    'EscapeRoom.py',
+    'app.py',
+    'EscapeRoom3.py'
 }
 
 for directory, python_file in directories.items():
@@ -74,4 +34,3 @@ for directory, python_file in directories.items():
         os.system(f"python {python_file_path}")
     else:
         print(f"{directory} is not a valid directory")
->>>>>>> 25d6adfaa75e584c5bedba12eb7786bfdcf6f206
